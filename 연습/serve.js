8d003f1d928e9de7237d6cdbf263d5497ee022e5
Kimjw111 <whale960105@gmail.com>
2023-06-26 16:43:33 +0900

    tpj

--- 연습/serve.js
+++ 연습/serve.js
@@ -5,14 +5,16 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
 // 정적 파일 제공
-app.use(express.static(__dirname + "/public"));
-
-app.get("/", (req, res) => {
+app.use(express.static(__dirname + "/"));
+app.get("/chat", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
 // 로그인한 사용자 저장을 위한 객체
 const users = {};
+
+// 대화 기록 저장을 위한 배열
+const conversationHistory = [];
 
 // 채팅방 연결
 io.on("connection", (socket) => {
@@ -38,7 +40,8 @@ io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
     const sender = users[socket.id].username;
     let message = msg.message;
-
+    // 전체 채팅 메시지 전송
+    io.emit("chat message", { message: message, from: sender });
     // 귓속말 처리
     if (msg.to) {
       const receiverId = Object.keys(users).find(
@@ -55,10 +58,39 @@ io.on("connection", (socket) => {
       }
     } else {
       message = `${sender}: ${message}`;
-    }
 
-    // 전체 채팅 메시지 전송
-    io.emit("chat message", { message: message, from: sender });
+      // 챗봇 응답 처리
+      if (!users[socket.id].isAnonymous) {
+        const triggerPhrases = [
+          "!명령어",
+          "안녕",
+          "반가워",
+          "좋은하루",
+          "날씨",
+          "잘가",
+        ];
+        const botResponses = [
+          "명령어 : 안녕, 반가워, 좋은하루, 날씨, 잘가",
+          "안녕하세요!",
+          "만나서 반가워요!",
+          "네, 좋은 하루 되세요!",
+          "오늘은 맑네요. 축구 한판 어떠신가요??!!",
+          "안녕히 가세요!",
+        ];
+
+        // 사용자가 보낸 메시지가 챗봇의 트리거 문구와 일치하는지 확인
+        const userMessage = msg.message;
+        const triggerIndex = triggerPhrases.indexOf(userMessage);
+
+        if (triggerIndex !== -1) {
+          const botResponse = botResponses[triggerIndex];
+          io.emit("chat message", {
+            message: ` ${botResponse}`,
+            from: "Chatbot",
+          });
+        }
+      }
+    }
   });
 
   // 연결 해제 이벤트 처리
